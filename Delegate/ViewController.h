c86111141caad069f1465e655000d447300d528d
EnzoFurmenti <ducheenzo@mail.ru>
2016-07-28 07:56:28 +0300

    Initial Commit

---
+++ Delegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegate
+//
+//  Created by EnzoF on 28.07.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
